fd1145f7c787a24176cd278c1ea2e316a19ca7ba
Samuel Trevan <samuel.trevan@gmail.com>
2022-11-29 14:53:46 -0600

    Phasse One Complete

--- TeamMember.cs
+++ TeamMember.cs
@@ -4,8 +4,14 @@ namespace PlanYourHeist
 {
     public class TeamMember
     {
-        public string Name {get; set;}
-        public int SkillLevel {get; set;}
-        public decimal CourageFactor {get; set;}
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public decimal CourageFactor { get; set; }
+
+
+        public string Stringify()
+        {
+            return ($"Name: {Name} Skill Level: {SkillLevel} Courage Factor: {CourageFactor}");
+        }
     }
 }
