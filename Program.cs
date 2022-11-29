aa5fa4e66ba88a98561bbfbabb69f54022d1bfe7
Morganero <morganero1@gmail.com>
2022-11-29 15:28:05 -0600

    phasetwo done

--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace PlanYourHeist
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TeamMember person1 = new TeamMember();
+            List<TeamMember> teamMembers = new List<TeamMember>();
             Console.WriteLine("Plan Your Heist B)");
-            Console.WriteLine("Enter team member's name");
-            person1.Name = Console.ReadLine();
-            Console.WriteLine("Enter team member's Skill Level between 1-10: ");
-            person1.SkillLevel = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter team member's courage factor between 0.0-2.0: ");
-            person1.CourageFactor = decimal.Parse(Console.ReadLine());
-            Console.WriteLine(person1.Stringify());
+            while(true)
+            {           
+                TeamMember person1 = new TeamMember();
+                Console.WriteLine("Enter team member's name");
+                person1.Name = Console.ReadLine();
+                if (person1.Name == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Enter team member's Skill Level between 1-10: ");
+                person1.SkillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter team member's courage factor between 0.0-2.0: ");
+                person1.CourageFactor = decimal.Parse(Console.ReadLine());
+                teamMembers.Add(person1);                 
+            };
+            Console.WriteLine($"{teamMembers.Count} team members in team.");
+            foreach (TeamMember teamMember in teamMembers)
+            {
+                Console.WriteLine($"{teamMember.Stringify()}");
+            }
         }
     }
 }
