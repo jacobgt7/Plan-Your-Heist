2e6af2daa80970eb7cc4573f42536db09f3aea0d
Simone Marcantel <simonemarcantel04@gmail.com>
2022-11-29 14:27:37 -0600

    phase one

---
+++ TeamMember.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlanYourHeist
+{
+    public class TeamMember
+    {
+        public string Name {get; set;}
+        public int SkillLevel {get; set;}
+        public decimal CourageFactor {get; set;}
+    }
+}
